60dfbc3aac3973020c24cb0c4e69d4803b2dc5c3
QOZU <64510789+QOZU@users.noreply.github.com>
2025-08-06 03:38:09 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/objectmodel/main.go
@@ -0,0 +1,23 @@
+// cmd/objectmodel/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"objectmodel/internal/objectmodel"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := objectmodel.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
